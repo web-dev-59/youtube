fc290fbb4581eb732f491ab2df78d0f0f50df4df
Deniskot59 <63236749+Deniskot59@users.noreply.github.com>
2021-04-09 14:46:58 +0300

    добавил скролл

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.channel__slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
